6dbb5186ff1354387ab3174984518fa3688c9825
cami oliva <lamica.olivares@gmail.com>
2024-08-14 13:44:49 -0400

    add style bootstrap

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,6 +8,6 @@
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
 30.times do |i|
-    @tweets = Tweet.create(username: Faker::Lorem.word , description: Faker::Lorem.paragraph)
+    @tweets = Tweet.create(username: Faker::Twitter.user[:name] , description: Faker::Lorem.paragraph)
     print @tweets 
 end
